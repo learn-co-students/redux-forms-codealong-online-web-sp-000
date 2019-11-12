0de386be177c07c19bc9a200d8435324dc9501db
github username <github email address>
2019-11-12 16:26:40 -0500

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
 
 class CreateTodo extends Component {
+
+  state = {
+    text:''
+  }
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.addToDo(this.state)
+  }
+
   render() {
-    return(
+    return (
       <div>
-        Create Todo Component
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <p>
+            <label>add todo</label>
+            <input type="text" value={this.state.text} onChange={(event)=>this.handleChange(event)} />
+          </p>
+          <input type="submit" />
+        </form>
       </div>
-    )
+    );
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addToDo: (formData) => dispatch({type: "ADD_TODO", payload: formData})
   }
 }
-
-export default CreateTodo;
+export default connect(null, mapDispatchToProps)(CreateTodo);
