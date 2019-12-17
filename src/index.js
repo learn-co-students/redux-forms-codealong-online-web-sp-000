ff7e2ec2715dbb1241826268a706b17a204b1aa2
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-12-17 11:58:14 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import manageTodo from './reducers/manageTodo';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-let store = createStore(manageTodo);
+class CreateTodo extends Component {
+  state = {
+    text: ''
+  };
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+  handleChange = event => {
+    this.setState({
+      text: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addTodo(this.state);
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <p>
+            <label>add todo</label>
+              <input
+                type="text"
+                onChange={event => this.handleChange(event)}
+                value={this.state.text}
+              />
+          </p>
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addTodo: formData => dispatch({ type: 'ADD_TODO', payload: formData })
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CreateTodo);
