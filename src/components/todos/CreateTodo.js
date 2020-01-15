46bd7081c8dd523a381e4fffcd3b7b6c8ebd2703
Brett Saxon <brettcsaxon@gmail.com>
2020-01-14 19:52:21 -0500

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react'
-
+import { connect } from 'react-redux';
 class CreateTodo extends Component {
+
+  state = {
+    text: ''
+  }
+
+  handleChange = event => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.props.dispatch({ type: 'ADD_TODO', payload: this.state })
+  }
+
   render() {
     return(
       <div>
-        Create Todo Component
+        <form onSubmit={ event => this.handleSubmit(event) }>
+          <p>
+            <label>add todo</label>
+            <input type='text' onChange={this.handleChange} value={this.state.text} />
+          </p>
+          <input type='submit' />
+        </form>
+        {this.state.text}
       </div>
     )
   }
 }
 
-export default CreateTodo;
+
+//longhand way
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addTodo: formData => dispatch({ type: 'ADD_TODO', payload: formData})
+//   }
+// }
+
+//if not given any arguments, connect will return dispatch as a prop
+// to the component we're wrapping w/ connect. 
+
+//Alternative way to write it. 
+export default connect()(CreateTodo)
+
+// long hand way below. 
+// export default connect(null, mapDispatchToProps)(CreateTodo);
