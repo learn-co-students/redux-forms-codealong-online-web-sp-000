87f69325626996f4341b2c963459d14275a6728e
Blythe Pollard <blytheepollard@gmail.com>
2020-04-28 16:05:02 -0600

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
 
 class CreateTodo extends Component {
-  render() {
+  state = {
+      text: '',
+    };
+  
+   
+  handleChange = event => {
+    this.setState({
+      text: event.target.value
+    });
+  }; //turn this into arrow function rather than class method 
+  //so that handleChange will always be bound to instance of component
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addTodo(this.state)
+  }
+   
+  render(){
     return(
       <div>
-        Create Todo Component
+        <form onSubmit={ event => this.handleSubmit(event)}>
+          <p>
+            <label>add todo</label>
+            <input type="text" onChange={(event) => this.handleChange(event)} value={this.state.text}/>
+          </p>
+          <input type="submit" />
+        </form>
+        {this.state.text}
       </div>
-    )
-  }
+    );
+  } 
 }
 
-export default CreateTodo;
+const mapDispatchToProps = dispatch => {
+  return {
+    addTodo: formData => dispatch({ type: 'ADD_TODO', payload: formData })
+  };
+};
+
+export default connect(null, mapDispatchToProps)(CreateTodo);
+//not worried about mapping state because don't need state here- it is keeping its own
+
+//export default CreateTodo;
