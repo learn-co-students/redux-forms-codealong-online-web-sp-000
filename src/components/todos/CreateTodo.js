8a2d5e9ed210508a9928045694ebe8c49734e130
ocalvario <oswald.calvario@gmail.com>
2020-08-02 22:48:58 -0400

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,44 +1,41 @@
-
 import React, { Component } from 'react'
 
 class CreateTodo extends Component {
-
   constructor() {
     super();
     this.state = {
-      text: '',
-    };
+      text: ''
+    }
   }
 
-  handleChange(event){
+  handleChange = (event) => {
     this.setState({
       text: event.target.value
     })
-  };
+  }
 
-  handleSubmit(event) {
+  handleSubmit = (event) => {
     event.preventDefault();
     this.props.store.dispatch({
       type: 'ADD_TODO',
-      todo: this.state,
-    });
+      todo: this.state
+    })
   }
 
-  render(){
+  render() {
     return(
       <div>
-        <form onSubmit={(event) => this.handleSubmit(event)}>
+        <form onSubmit={this.handleSubmit} >
           <p>
             <label>add todo</label>
-            <input type="text" onChange={(event) => this.handleChange(event)}/>
+            <input type="text" onChange={this.handleChange} />
           </p>
           <input type="submit" />
         </form>
         {this.state.text}
       </div>
-    );
+    )
   }
-};
-
+}
 
 export default CreateTodo;
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,10 +1,7 @@
-export default function manageTodo(state = {
-  todos: [],
-}, action) {
+export default function manageTodo(state = {todos: []}, action) {
   switch (action.type) {
-    case 'ADD_TODO':
-      return { todos: state.todos.concat(action.todo.text) };
-
+    case "ADD_TODO":
+      return {todos: state.todos.concat(action.todo.text)}
     default:
       return state;
   }
