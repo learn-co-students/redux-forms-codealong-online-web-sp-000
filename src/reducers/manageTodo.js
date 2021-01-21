0d561e5d2814f3780e37e5c4c1250b225fa2a3dd
Snowing222 <snowing222@gmail.com>
2021-01-21 15:38:30 -0500

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -3,6 +3,16 @@ export default function manageTodo(state = {
 }, action) {
 
   console.log("reducer received this action:", action);
+  switch(action.type){
+    case 'AddToDo':
+      console.log(state.todos.concat(action.todo))
+      return {
+       
+        todos: state.todos.concat(action.todo)
+      }
+    default:
+      return state
+  }
 
-  return state;
+
 }
