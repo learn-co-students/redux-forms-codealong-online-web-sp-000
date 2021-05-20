4d903f173494fe08221dccef4f3f6ee861081cfa
Austin Rhoads <austinrrhoads@ymail.com>
2021-05-20 14:34:31 -0500

    hep concat

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
 
 class CreateTodo extends Component {
+
+  state = {
+    text: "",
+  }
+
+handelChange = (e) => {
+  this.setState({
+    text: e.target.value
+  })
+}
+
+handelSubmit = (e) => {
+  e.preventDefault();
+  this.props.addTodo(this.state);
+}
+
+
   render() {
     return(
       <div>
         Create Todo Component
+        <form onSubmit={e => this.handelSubmit(e)}>
+          <p>
+            <label>add todo</label>
+            <input type="text" onChange={ this.handelChange} value={this.state.text} />
+          </p>
+          <input type="submit" />
+        </form>
+        {this.state.text}
       </div>
     )
   }
 }
 
-export default CreateTodo;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addTodo: (formData) => dispatch({type: "ADD_TODO", payload: formData})
+  }
+}
+
+export default connect(null, mapDispatchToProps) (CreateTodo);
